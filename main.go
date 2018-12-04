0994f8add1004884f802398e057b262436212bdc
Nader Eloshaiker <nader.eloshaiker@gmail.com>
2018-12-04 11:35:47 +1100

    Section 5 Lecture 50 - interating maps

--- main.go
+++ main.go
@@ -4,18 +4,24 @@ import "fmt"
 
 func main() {
 	//var colors map[string]string
+	//colors := make(map[string]string)
 
-	colors := make(map[string]string)
+	//colors["white"] = "#ffffff"
+	//delete(colors, "white")
 
-	colors["white"] = "#ffffff"
-	delete(colors, "white")
-
-	//colors := map[string]string{
-	//	"red": "#ff0000",
-	//	"green": "#00ff00",
-	//	"blue": "0000ff",
-	//}
+	colors := map[string]string{
+		"red": "#ff0000",
+		"green": "#00ff00",
+		"blue": "0000ff",
+		"white": "ffffff",
+	}
 
 
-	fmt.Println(colors)
+	printMap(colors)
+}
+
+func printMap(m map[string]string) {
+	for key, value := range m {
+		fmt.Println("key:" , key, "value:", value)
+	}
 }
